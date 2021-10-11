8162d95b81d417f04bf43002a5aea08c922778f7
FranMedic <m.francisca.medico@gmail.com>
2021-10-11 20:08:16 +0200

    added class Personaes

---
+++ public/javascript/Personajes.js
@@ -0,0 +1,28 @@
+class Personajes {
+  nombre;
+  edad;
+  estado = "vivo";
+  serie = "juego de trono";
+  frase;
+
+  static familia = {
+    targaryen: "casa Targaryen",
+    lannister: "casa Lannister",
+    baratheon: "casa Baratheon",
+    aguasnegras: "casa Aguas Negras",
+  };
+
+  constructor(nombre, familia, edad) {
+    this.nombre = nombre;
+    this.familia = familia;
+    this.edad = edad;
+  }
+  comunicarse() {
+    return this.frase;
+  }
+  morir() {
+    this.estado = "muerto";
+  }
+}
+
+export default Personajes;
