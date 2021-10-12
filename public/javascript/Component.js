b0cc0e975faafe9984b3d75e70b89c3ab347223c
FranMedic <m.francisca.medico@gmail.com>
2021-10-12 18:07:42 +0200

    added class Component

---
+++ public/javascript/Component.js
@@ -0,0 +1,20 @@
+import Asesor from "./Asesor.js";
+import Escudero from "./Escudero.js";
+import Luchador from "./Luchador.js";
+import Personajes from "./Personajes.js";
+import Rey from "./Rey.js";
+
+class Component {
+  element;
+  parentElement;
+
+  constructor(parentElement, className, htmlTag = "div") {
+    this.element = document.createElement(htmlTag);
+    this.element.className = className;
+    this.parentElement = parentElement;
+
+    this.parentElement.append(this.parentElement);
+  }
+}
+
+export default Component;
